c427c4a93f8a7289f340ea0c8f76595bf3ae7eb5
Madmax <113833416+NANDILLONMaxence@users.noreply.github.com>
2023-12-04 18:42:10 +0100

    Update 000_Bug-CATCHER.sh
    Fixe repository to Apt sources

--- 000_Bug-CATCHER.sh
+++ 000_Bug-CATCHER.sh
@@ -8,13 +8,32 @@ apt update -y && apt upgrade -y
   # - Cependant, veuillez noter que les noms des paquets peuvent varier en fonction de votre distribution.
   # - Assurez-vous d'avoir les paquets requis pour votre distribution (Linux)
 
+# install sudo 
+apt install sudo 
+
 # curl :
 apt install -y  curl 
+
 # Certificates :
 apt install -y apt-transport-https ca-certificates curl
 
 # propriétés-logicielles-communes:
 apt install -y software-properties-common
+
+## APT Docker pour Debian
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
 
 # Étape 3 : Ajoutez le packet gpg
   # Outil qui permet de chiffrer/déchiffrer mais aussi de signer 
